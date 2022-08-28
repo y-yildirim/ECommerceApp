81fdd03f05cb7e5fa1f60f96b18645572ced805d
y-yildirim <y.yildirim.ce@gmail.com>
2022-08-28 11:04:09 +0300

    Fix disposed context

--- Infrastructure/ECommerceApp.Persistence/ServiceRegistration.cs
+++ Infrastructure/ECommerceApp.Persistence/ServiceRegistration.cs
@@ -14,13 +14,13 @@ namespace ECommerceApp.Persistence
     {
         public static void AddPersistanceServices(this IServiceCollection services)
         {
-            services.AddDbContext<ECommerceAppDbContext>(options => options.UseNpgsql(Configuration.ConnectionString), ServiceLifetime.Singleton);
-            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
-            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
-            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
-            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
-            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
-            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
+            services.AddDbContext<ECommerceAppDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
