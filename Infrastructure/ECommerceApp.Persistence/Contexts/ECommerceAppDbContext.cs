9547fadb32db112bab6663c1c44c38234bf50dfa
y-yildirim <y.yildirim.ce@gmail.com>
2022-08-29 00:45:28 +0300

    Add interceptor

--- Infrastructure/ECommerceApp.Persistence/Contexts/ECommerceAppDbContext.cs
+++ Infrastructure/ECommerceApp.Persistence/Contexts/ECommerceAppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using ECommerceApp.Domain.Entities;
+using ECommerceApp.Domain.Entities.Common;
 using Microsoft.EntityFrameworkCore;
 
 namespace ECommerceApp.Persistence.Contexts
@@ -12,5 +13,22 @@ namespace ECommerceApp.Persistence.Contexts
         public DbSet<Customer> Customers { get; set; } = default!;
         public DbSet<Order> Orders { get; set; } = default!;
         public DbSet<Product> Products { get; set; } = default!;
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var entry in entries)
+            {
+                _ = entry.State switch
+                {
+                    EntityState.Added => entry.Entity.CreatedAt = DateTime.UtcNow,
+                    EntityState.Modified => entry.Entity.UpdatedAt = DateTime.UtcNow,
+                    _ => throw new InvalidOperationException(nameof(entry.State))
+                };
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
