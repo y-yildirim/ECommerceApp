3ca0a58451bf64b482628c6ebc158d20ea666df5
y-yildirim <y.yildirim.ce@gmail.com>
2022-08-28 11:37:26 +0300

    Add tracking performance optimization

--- Infrastructure/ECommerceApp.Persistence/Repositories/ReadRepository.cs
+++ Infrastructure/ECommerceApp.Persistence/Repositories/ReadRepository.cs
@@ -17,12 +17,31 @@ namespace ECommerceApp.Persistence.Repositories
 
         public DbSet<TEntity> Table => _dbContext.Set<TEntity>();
 
-        public Task<List<TEntity>> GetAll() => Table.ToListAsync();
+        public Task<List<TEntity>> GetAll(bool tracking = true)
+        {
+            var query = Table.AsQueryable();
+            if (!tracking) query = query.AsNoTracking();
+            return query.ToListAsync();
+        }
 
-        public Task<List<TEntity>> GetWhere(Expression<Func<TEntity, bool>> method) => Table.Where(method).ToListAsync();
+        public Task<List<TEntity>> GetWhere(Expression<Func<TEntity, bool>> method, bool tracking = true)
+        {
+            var query = Table.Where(method);
+            if (!tracking) query = query.AsNoTracking();
+            return query.ToListAsync();
+        }
 
-        public async Task<TEntity?> GetSingleAsync(Expression<Func<TEntity, bool>> method) => await Table.FirstOrDefaultAsync(method);
+        public async Task<TEntity?> GetSingleAsync(Expression<Func<TEntity, bool>> method, bool tracking = true)
+        {
+            if (!tracking) return await Table.AsNoTracking().FirstOrDefaultAsync(method);
+            return await Table.FirstOrDefaultAsync(method);
+        }
 
-        public async Task<TEntity?> GetByIdAsync(string id) => await Table.FindAsync(Guid.Parse(id));
+        public async Task<TEntity?> GetByIdAsync(string id, bool tracking = true)
+        {
+            var guidId = Guid.Parse(id);
+            if (!tracking) return await Table.AsNoTracking().SingleAsync(e => e.Id == guidId);
+            return await Table.FindAsync(guidId);
+        }
     }
 }
